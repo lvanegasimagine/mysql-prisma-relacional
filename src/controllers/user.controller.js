f5789b1c992782d4d2a8298eb661bd07407b3f1e
Luis Vanegas <lvanegas1429@gmail.com>
2022-02-18 09:52:45 -0600

    Peticiones CRUD de users finalizada

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,20 +1,12 @@
 const { PrismaClient } = require("@prisma/client");
 
 const { user } = new PrismaClient();
-const prisma = new PrismaClient();
 
 const getUsers = async (req, res) => {
   try {
-    const users = await user
-      .findMany
-      //Todo: Me permite traer solo los campos que quiero
-      // {
-      //     select: {
-      //         name: true,
-      //         post: true
-      //     }
-      // }
-      ();
+    const users = await user.findMany({
+      // take: 5
+    });
     res.status(200).json({
       status: true,
       data: users,
@@ -64,7 +56,92 @@ const createUser = async (req, res) => {
     }
 };
 
+const getUserById = async (req,res) => {
+      
+  try {
+    const { id_user } = req.params;
+
+    const result = await user.findUnique(
+      {
+        where: { id: Number(id_user)},
+      }
+    )
+
+    if(!result){
+      return res.status(404).json({
+        status: true,
+        message: 'No Existe este usuario'
+      }); 
+    }
+
+    res.status(200).json({
+      status: true,
+      data: result
+    })
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      error: error
+    })
+  }
+      
+      
+}
+
+const updateUser = async (req, res) => {
+    try {
+      const { username } = req.body;
+      const { id_user } = req.params;
+
+      const updateUser = await user.update({
+        where:{
+          id:  Number(id_user)
+        },
+        data: {
+          username
+        }
+      });
+
+      res.status(200).json({
+        status: true,
+        data: updateUser
+      })
+    } catch (error) {
+      res.status(500).json({
+        status: false,
+        error: error
+      })
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+      const { id_user } = req.params;
+
+      const data = await user.delete({
+        where: {id: Number(id_user)}
+      });
+
+      res.status(200).json({
+        status: true,
+        data,
+        message: 'Usuario Eliminado'
+      })
+    } catch (error) {
+      const {meta} = error;
+      res.status(500).json({
+        status: false,
+        error: meta.cause
+      })
+    }
+}
+
+
+
 module.exports = {
   getUsers,
+  getUserById,
   createUser,
+  updateUser,
+  deleteUser
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,11 @@
-const { getUsers, createUser } = require('../controllers/user.controller');
+const { getUsers, createUser, getUserById, updateUser ,deleteUser } = require('../controllers/user.controller');
 
 const router = require('express').Router();
 
-router.get('/', getUsers)
-router.post('/', createUser)
+router.get('/', getUsers);
+router.get('/:id_user', getUserById);
+router.post('/', createUser);
+router.put('/:id_user', updateUser);
+router.delete('/:id_user', deleteUser);
+
 module.exports = router;
